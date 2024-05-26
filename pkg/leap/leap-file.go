e990bbdcecf4e264adc21c95de4ce2edc56b7604
Vitaly Grinberg <v.g@redhat.com>
2024-05-26 08:28:56 -0400

    wip

--- pkg/leap/leap-file.go
+++ pkg/leap/leap-file.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"bytes"
 	"context"
+	"crypto/sha1"
 	"fmt"
 	"html/template"
 	"os"
@@ -12,7 +13,6 @@ import (
 	"strings"
 	"time"
 
-	leaphash "github.com/facebook/time/leaphash"
 	"github.com/golang/glog"
 	"github.com/openshift/linuxptp-daemon/pkg/ublox"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -22,11 +22,12 @@ import (
 const (
 	defaultLeapFileName    = "leap-seconds.list"
 	defaultLeapFilePath    = "/usr/share/zoneinfo"
-	gpsLeapToUtcLeap       = 37 - 18
+	gpsToTaiDiff           = 19
 	curreLsValidMask       = 0x1
 	timeToLsEventValidMask = 0x2
 	leapSourceGps          = 2
 	leapConfigMapName      = "leap-configmap"
+	MaintenancePeriod      = time.Minute * 1
 )
 
 type LeapManager struct {
@@ -41,6 +42,8 @@ type LeapManager struct {
 	namespace string
 	// Leap file structure
 	leapFile LeapFile
+	// Retry configmap update if failed
+	retryUpdate bool
 }
 
 type LeapEvent struct {
@@ -102,8 +105,15 @@ func ParseLeapFile(b []byte) (*LeapFile, error) {
 }
 
 func (l *LeapManager) RenderLeapData() (*bytes.Buffer, error) {
-	templateFile := "templates/leap-seconds.list.template"
-	templ, err := template.ParseFiles(templateFile)
+	templateStr := `# Do not edit
+# This file is generated automatically by linuxptp-daemon
+#$	{{ .UpdateTime }}
+#@	{{ .ExpirationTime }}
+{{ range .LeapEvents }}{{ .LeapTime }}     {{ .LeapSec }}    {{ .Comment }}
+{{ end }}
+#h	{{ .Hash }}`
+
+	templ, err := template.New("leap").Parse(templateStr)
 	if err != nil {
 		return nil, err
 	}
@@ -135,11 +145,19 @@ func (l *LeapManager) PopulateLeapData() error {
 			return err
 		}
 		l.leapFile = *leapData
-
+		// Set expiration time to 2036
+		exp := time.Date(2035, time.January, 1, 0, 0, 0, 0, time.UTC)
+		start := time.Date(1970, time.January, 1, 0, 0, 0, 0, time.UTC)
+		expSec := int(exp.Sub(start).Seconds())
+		l.leapFile.ExpirationTime = fmt.Sprint(expSec)
+		data, err := l.RenderLeapData()
+		if err != nil {
+			return err
+		}
 		if len(cm.Data) == 0 {
 			cm.Data = map[string]string{}
 		}
-		cm.Data[nodeName] = string(b)
+		cm.Data[nodeName] = data.String()
 		_, err = l.client.CoreV1().ConfigMaps(l.namespace).Update(context.TODO(), cm, metav1.UpdateOptions{})
 		if err != nil {
 			return err
@@ -161,125 +179,82 @@ func (l *LeapManager) SetLeapFile(leapFile string) {
 
 func (l *LeapManager) Run() {
 	glog.Info("starting Leap file manager")
+	ticker := time.NewTicker(MaintenancePeriod)
 	for {
 		select {
 		case v := <-l.UbloxLsInd:
 			l.HandleLeapIndication(&v)
 		case <-l.Close:
 			return
+		case <-ticker.C:
+			if l.retryUpdate {
+				l.UpdateLeapConfigmap()
+			}
+			// TODO: if current time is within -12h ... +60s from leap event:
+			// Send PMC command
 		}
 	}
 }
 
-func GetLastLeapEventFromFile(fp string) (*time.Time, int, error) {
-	b, err := os.ReadFile(fp)
-	if err != nil {
-		return nil, 0, err
-	}
-	// TODO: Add integrity check
-	lines := strings.Split(string(b), "\n")
-	var leapSection = false
-	for i := 0; i < len(lines); i++ {
-		if !strings.HasPrefix(lines[i], "#") {
-			leapSection = true
-		}
-		if leapSection && strings.HasPrefix(lines[i], "#") {
-			// line i-1 contains the last leap
-			event := strings.Fields(lines[i-1])
-			if len(event) < 2 {
-				return nil, 0, fmt.Errorf("failed to get last Leap event from file %s: %v", fp, err)
-			}
-			delta, err := time.ParseDuration(event[0] + "s")
-			if err != nil {
-				return nil, 0, fmt.Errorf("failed to convert last Leap event duration %s: %v", event[0], err)
-			}
+// AddLeapEvent appends a new leap event to the list of leap events
+func (l *LeapManager) AddLeapEvent(leapTime time.Time,
+	leapSec int, expirationTime time.Time, currentTime time.Time) {
 
-			startTime := time.Date(1900, time.January, 1, 0, 0, 0, 0, time.UTC)
-			leapEventTime := startTime.Add(delta)
-
-			leapSec, err := strconv.Atoi(event[1])
-			if err != nil {
-				return nil, 0, fmt.Errorf("failed to convert Leap seconds to int %s: %v", event[1], err)
-			}
-			return &leapEventTime, leapSec, nil
-		}
-	}
-	return nil, 0, fmt.Errorf("can't find last Leap event in file %s: %v", fp, err)
-}
-
-func CheckLeapFileIntegrity(filePath string) error {
-	var hashOnFile string
-	b, err := os.ReadFile(filePath)
-	if err != nil {
-		return err
-	}
-	lines := strings.Split(string(b), "\n")
-
-	// going backwards from the end of file
-	for i := len(lines) - 1; i >= 0; i-- {
-		// line starts from the hash sign
-		if strings.HasPrefix(lines[i], "#h") {
-			hashOnFile = strings.Join(strings.Fields(lines[i])[1:], " ")
-			break
-		}
-	}
-	hash := leaphash.Compute(string(b))
-	if strings.Compare(hash, hashOnFile) == 0 {
-		return nil
-	}
-	return fmt.Errorf("integrity error: %s - on Leap file, %s - computed",
-		hashOnFile, hash)
-}
-
-// AddLeapEvent appends a leap event to the leap-file.list
-func AddLeapEvent(filepath string, leapTime time.Time,
-	leapSec int, expirationTime time.Time, currentTime time.Time) error {
 	startTime := time.Date(1900, time.January, 1, 0, 0, 0, 0, time.UTC)
-	leap := int(leapTime.Sub(startTime).Seconds())
-	modified := int(currentTime.Sub(startTime).Seconds())
-	expired := int(expirationTime.Sub(startTime).Seconds())
-	//fmt.Printf("leap %d, mod %d, exp %d", leap, modified, expired)
-	b, err := os.ReadFile(filepath)
+	leapTimeTai := int(leapTime.Sub(startTime).Seconds())
+
+	ev := LeapEvent{
+		LeapTime: fmt.Sprint(leapTimeTai),
+		LeapSec:  leapSec,
+		Comment: fmt.Sprintf("# %v %v %v",
+			leapTime.Day(), leapTime.Month().String()[:3], leapTime.Year()),
+	}
+	l.leapFile.LeapEvents = append(l.leapFile.LeapEvents, ev)
+	l.RehashLeapData()
+
+}
+
+func (l *LeapManager) RehashLeapData() {
+	data := fmt.Sprint(l.leapFile.UpdateTime) + fmt.Sprint(l.leapFile.ExpirationTime)
+
+	for _, ev := range l.leapFile.LeapEvents {
+		data += fmt.Sprint(ev.LeapTime) + fmt.Sprint(ev.LeapSec)
+	}
+
+	// checksum
+	hash := fmt.Sprintf("%x", sha1.Sum([]byte(data)))
+
+	var groupedHash string
+	// group checksum by 8 characters
+	for i := 0; i < 5; i++ {
+		if groupedHash != "" {
+			groupedHash += " "
+		}
+		groupedHash += hash[i*8 : (i+1)*8]
+	}
+	l.leapFile.Hash = groupedHash
+}
+
+func (l *LeapManager) UpdateLeapConfigmap() {
+	data, err := l.RenderLeapData()
 	if err != nil {
-		return err
+		glog.Error("Leap: ", err)
+		return
 	}
-	lines := strings.Split(string(b), "\n")
+	cm, err := l.client.CoreV1().ConfigMaps(l.namespace).Get(context.TODO(), leapConfigMapName, metav1.GetOptions{})
 
-	out := []string{}
-	var leapSection = false
-	for i := 0; i < len(lines); i++ {
-		if strings.HasPrefix(lines[i], "#$") {
-			// "#$" - last modification time
-			lines[i] = strings.Join([]string{"#$", fmt.Sprintf("%d", modified)}, "\t")
-		} else if strings.HasPrefix(lines[i], "#@") {
-			// "#@" - the expiration time of the file
-			lines[i] = strings.Join([]string{"#@", fmt.Sprintf("%d", expired)}, "\t")
-		}
-
-		if !strings.HasPrefix(lines[i], "#") {
-			leapSection = true
-		}
-		if leapSection && strings.HasPrefix(lines[i], "#") {
-			// Insert the new leap event
-			out = append(out, strings.Join(
-				[]string{fmt.Sprintf("%d", leap),
-					"    ",
-					fmt.Sprintf("%d", leapSec),
-					"    ",
-					"#", fmt.Sprintf("%v %v %v",
-						leapTime.Day(), leapTime.Month().String()[:3], leapTime.Year())}, " "))
-			leapSection = false
-		}
-		if strings.HasPrefix(lines[i], "#h") {
-			// "#h" - the hash
-			// Compute the hash and add it to the end of file
-			s := strings.Join(out, "\n")
-			hash := leaphash.Compute(s)
-			lines[i] = strings.Join([]string{"#h", hash}, "\t")
-		}
-		out = append(out, lines[i])
+	if err != nil {
+		l.retryUpdate = true
+		return
 	}
-	return os.WriteFile(filepath, []byte(strings.Join(out, "\n")), 0644)
+	nodeName := os.Getenv("NODE_NAME")
+	cm.Data[nodeName] = data.String()
+	_, err = l.client.CoreV1().ConfigMaps(l.namespace).Update(context.TODO(), cm, metav1.UpdateOptions{})
+	if err != nil {
+		l.retryUpdate = true
+		return
+	}
+	l.retryUpdate = false
 }
 
 // HandleLeapIndication handles NAV-TIMELS indication
@@ -287,47 +262,37 @@ func AddLeapEvent(filepath string, leapTime time.Time,
 // If leap event is closer than 12 hours in the future,
 // GRANDMASTER_SETTINGS_NP dataset will be updated with
 // the up to date leap second information
-
 func (l *LeapManager) HandleLeapIndication(data *ublox.TimeLs) {
 	glog.Infof("Leap indication: %+v", data)
 	if data.SrcOfCurrLs != leapSourceGps {
 		glog.Info("Discarding Leap event not opriginating from GPS")
 		return
 	}
-	_, leapSecOnFile, err := GetLastLeapEventFromFile(l.LeapFilePath)
-	if err != nil {
-		glog.Error("Leap file error: ", err)
-		return
-	}
+	// Current leap seconds on file
+	leapSecOnFile := l.leapFile.LeapEvents[len(l.leapFile.LeapEvents)-1].LeapSec
 
 	validCurrLs := (data.Valid & curreLsValidMask) > 0
 	validTimeToLsEvent := (data.Valid & timeToLsEventValidMask) > 0
-	expirationTime := time.Now().UTC().Add(time.Hour * 87660)
+	expirationTime := time.Date(2036, time.January, 1, 0, 0, 0, 0, time.UTC)
 	currentTime := time.Now().UTC()
 
 	if validTimeToLsEvent && validCurrLs {
-		leapSec := int(data.CurrLs) + gpsLeapToUtcLeap + int(data.LsChange)
-		if data.LsChange != 0 && data.TimeToLsEvent > -1 && data.TimeToLsEvent < 12*3600 {
-			// TODO: Run PMC command to announce the leap downstream
-			glog.Info("Leap event is within ", data.TimeToLsEvent, " sec.")
-		}
+		leapSec := int(data.CurrLs) + gpsToTaiDiff + int(data.LsChange)
 		if leapSec != leapSecOnFile {
 			// File update is needed
 			glog.Infof("Leap Seconds on file outdated: %d on file, %d + %d + %d in GNSS data",
-				leapSecOnFile, int(data.CurrLs), gpsLeapToUtcLeap, int(data.LsChange))
+				leapSecOnFile, int(data.CurrLs), gpsToTaiDiff, int(data.LsChange))
 			startTime := time.Date(1980, time.January, 6, 0, 0, 0, 0, time.UTC)
 			deltaHours, err := time.ParseDuration(fmt.Sprintf("%dh",
 				data.DateOfLsGpsWn*7*24+uint(data.DateOfLsGpsDn)*24))
 			if err != nil {
-				glog.Error("Leap:", err)
+				glog.Error("Leap: ", err)
 				return
 			}
 			leapTime := startTime.Add(deltaHours)
-			err = AddLeapEvent(l.LeapFilePath, leapTime, leapSec, expirationTime, currentTime)
-			if err != nil {
-				glog.Error("Leap :", err)
-				return
-			}
+			// Add a new leap event
+			l.AddLeapEvent(leapTime, leapSec, expirationTime, currentTime)
+			l.UpdateLeapConfigmap()
 		}
 	}
 }
--- pkg/leap/leap-file_test.go
+++ pkg/leap/leap-file_test.go
@@ -2,48 +2,38 @@ package leap
 
 import (
 	"bytes"
-	"fmt"
 	"os"
 	"testing"
 	"time"
 
+	leaphash "github.com/facebook/time/leaphash"
 	"github.com/openshift/linuxptp-daemon/pkg/ublox"
 	"github.com/stretchr/testify/assert"
 )
 
 func Test_AddLeapEvent(t *testing.T) {
 	leapFile := "testdata/leap-seconds.list"
-	testFile := "/tmp/leap-seconds.list"
 	leapTime := time.Date(2025, time.January, 1, 0, 0, 0, 0, time.UTC)
 	expirationTime := time.Date(2035, time.January, 1, 0, 0, 0, 0, time.UTC)
 	currentTime := time.Date(2024, time.May, 8, 0, 0, 0, 0, time.UTC)
 	leapSec := 38
-	// Create the test file
-	input, err := os.ReadFile(leapFile)
+	b, err := os.ReadFile(leapFile)
 	assert.Equal(t, nil, err)
-	err = os.WriteFile(testFile, input, 0644)
+	leapData, err := ParseLeapFile(b)
 	assert.Equal(t, nil, err)
-	err = AddLeapEvent(testFile, leapTime, leapSec, expirationTime, currentTime)
-	assert.Equal(t, nil, err)
-	// Compare files
-	generated, err := os.ReadFile(testFile)
-	assert.True(t, err == nil)
-	desired, err := os.ReadFile("testdata/leap-seconds.list.desired")
-	assert.Equal(t, nil, err)
-	assert.True(t, bytes.Equal(generated, desired))
-	// cleanup
-	err = os.Remove(testFile)
-	assert.Equal(t, nil, err)
-}
+	lm := &LeapManager{
+		UbloxLsInd: make(chan ublox.TimeLs),
+		Close:      make(chan bool),
+		leapFile:   *leapData,
+	}
+	lm.AddLeapEvent(leapTime, leapSec, expirationTime, currentTime)
 
-func Test_GetLastLeapEventFromFile(t *testing.T) {
-	leapFile := "testdata/leap-seconds.list"
-	td, leap, err := GetLastLeapEventFromFile(leapFile)
+	buf, err := lm.RenderLeapData()
 	assert.Equal(t, nil, err)
-	expectedLeap := time.Date(2017, time.January, 1, 0, 0, 0, 0, time.UTC)
-	assert.Equal(t, expectedLeap, *td)
-	assert.Equal(t, 37, leap)
-	fmt.Println(td)
+	hash := leaphash.Compute(buf.String())
+
+	assert.Equal(t, hash, lm.leapFile.Hash)
+
 }
 
 func Test_ParseLeapFile(t *testing.T) {
@@ -66,6 +56,7 @@ func Test_RenderLeapFile(t *testing.T) {
 		leapFile:   *leapData,
 	}
 	l, err := lm.RenderLeapData()
+
 	assert.Equal(t, nil, err)
 	desired, err := os.ReadFile("testdata/leap-seconds.list.rendered")
 	assert.Equal(t, nil, err)
