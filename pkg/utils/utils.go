2e0aa4c3c9ba3f4f27a149cc56ea2372ae0301a2
David Elie-Dit-Cosaque <deliedit@redhat.com>
2025-09-10 09:27:20 -0500

    Fix PHC Identifier

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -2,31 +2,13 @@ package utils
 
 import (
 	"regexp"
+
+	log "github.com/sirupsen/logrus"
 )
 
-// GetAlias generates a PHC (PTP Hardware Clock) identifier alias from network interface names.
-// It supports Intel and Mellanox naming formats with optional VLAN tags.
-//
-// Supported formats:
-//   - Intel: eth0 -> ethx, ens1f0 -> ens1fx, ens1f0.100 -> ens1fx.100
-//   - Mellanox: enP2s2f0np0 -> enP2s2fx, enP2s2f0np0.100 -> enP2s2fx.100
-//
-// For unsupported formats, returns the original interface name and logs an error.
-//
-// Parameters:
-//   - ifname: Network interface name (e.g., "ens1f0", "enP2s2f0np0", "eth0.100")
-//
-// Returns:
-//   - Alias string for PHC identification, or original name if format is unsupported
 func GetAlias(ifname string) string {
 	alias := ""
 	if ifname != "" {
-		// Check if it's already an aliased interface (ends with 'x' before optional VLAN)
-		alreadyAliasedPattern := regexp.MustCompile(`^(.+?)x(\..+)?$`)
-		if alreadyAliasedPattern.MatchString(ifname) {
-			return ifname
-		}
-
 		// Single regex to handle both Intel and Mellanox formats with optional VLAN
 		// Intel format: ens1f0, eth0, ens1f0.100 -> ens1fx, ethx, ens1fx.100
 		// Mellanox format: enP2s2f0np0, enP2s2f0np0.100 -> enP2s2fx, enP2s2fx.100
@@ -43,6 +25,7 @@ func GetAlias(ifname string) string {
 			}
 		} else {
 			// Interface doesn't match Intel or Mellanox format, return original interface name
+			log.Errorf("Interface %s does not match Intel or Mellanox naming format, using original interface name", ifname)
 			alias = ifname
 		}
 	}
