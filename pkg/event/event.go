9c5df5a12ceee506a7b10210f7ae4dbd619d13f6
nocturnalastro <micosta@redhat.com>
2025-10-30 12:39:31 +0000

    Remove data collection for insync condition

--- pkg/event/event.go
+++ pkg/event/event.go
@@ -1033,11 +1033,7 @@ func (e *EventHandler) GetData(cfgName string, processName EventSource) *Data {
 
 func (e *EventHandler) addEvent(event EventChannel) *DataDetails {
 	d := e.GetData(event.CfgName, event.ProcessName)
-	leadingIface := "NOT-SET-123"
-	if state, ok := e.clkSyncState[event.CfgName]; ok {
-		leadingIface = state.leadingIFace
-	}
-	d.AddEvent(event, leadingIface)
+	d.AddEvent(event)
 
 	// update if DPLL holdover is out of spec
 	e.updateSpecState(event)
